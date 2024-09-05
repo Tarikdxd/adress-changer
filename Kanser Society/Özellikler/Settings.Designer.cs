30530de70251880e35d2838749b3bb96f064f5fd
Tarık Tozlu <kanserbusiness@gmail.com>
2024-09-05 19:51:24 +0300

    Update Settings.Designer.cs

--- Kanser Society/Özellikler/Settings.Designer.cs
+++ Kanser Society/Özellikler/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Mustangs_Unbanner.Properties
+namespace Tarikdxd_Unbanner.Properties
 {
 
 
