d7c0e47d228b50093460a903c770a8a63889abe7
Tarık Tozlu <kanserbusiness@gmail.com>
2024-09-05 19:50:41 +0300

    Update Resources.Designer.cs

--- Kanser Society/Özellikler/Resources.Designer.cs
+++ Kanser Society/Özellikler/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Mustangs_Unbanner.Properties
+namespace Tarikdxd_Unbanner.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace Mustangs_Unbanner.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Mustangs_Unbanner.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Tarikdxd_Unbanner.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
